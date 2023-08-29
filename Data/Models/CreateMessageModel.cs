c83146541826cc97aab721b84fb195c86f142518
jbg <qudrl0284@gmail.com>
2023-08-29 18:34:07 +0900

    1234

--- Data/Models/CreateMessageModel.cs
+++ Data/Models/CreateMessageModel.cs
@@ -1,60 +1,71 @@
+using Newtonsoft.Json;
+
 namespace TempTitle.Data.ViewModel.CreateMessageModel
 {
     public class CreateMessageModel
     {
         // 거래소
-        public string exchange { get; set; }
+        public string Exchange { get; set; }
         // 계정
-        public string account { get; set; }
+        public string Account { get; set; }
         // 거래종목
-        public string symbol { get; set ; }
+        public string Symbol { get; set ; }
         // 거래마켓
-        public string market { get; set; }
+        public string Market { get; set; }
         // 주문유형
         // 지정가 : limit, 시장가 : market
-        public string type { get; set; }
+        public string Type { get; set; }
         // 주문방법
         // 매수 : buy, 매도 : sell
-        public string side { get; set; }
+        public string Side { get; set; }
         // 주문가격
         // LAST, BID, ASK, string(소수점포함 실수로 작성가능 / 숫자가 아니면 작성x하고 오류메시지 출력)
-        public string price { get; set; }
+        public string Price { get; set; }
         // 주문가격대비 퍼센트(%)
         // 선택사항 : 왼쪽 대비 싸게 매수할 경우 -n(%)을 입력(마이너스값)
-        public int price_pct { get; set; }
+        // 소수점 2번째 자리까지 가능
+        public decimal Price_Pct { get; set; }
 
         //===== 주문 수량 선택 =====
-        // 주문수량
-        public int? amount { get; set; }
+        // 주문수량(개수)
+        // 소수점 4번째 자리까지 가능
+        public decimal? Amount { get; set; }
         // 주문타입
         // 1INCH, KRW
-        public string? amount_type { get; set; }
+        public string? Amount_type { get; set; }
 
         // 기준자산
         // 잔액대비 : 없음, 총자산대비 : "equity" : "y"
-        public string? equity { get; set; }
-        // 기준자산 - 주문수량
-        public int? bal_pct { get; set; }
+        public string? Equity { get; set; }
+        public string? RawEquity
+        {
+            get => Equity;
+            set => Equity = value;
+        }
+        // 주문수량(%)
+        // 소수 2번째 자리까지 가능
+        public decimal? Bal_Pct { get; set; }
 
         //===== 선택 사항 =====
         // 미체결 주문 취소
         // 전부취소 : cancel, 매수주문만 : cancel-buy, 매도주문만 : cancel-sell
-        public string? open_order { get; set; }
+        public string? Open_Order { get; set; }
         // 동일주문 연속실행 방지
         // n분이내 유효 : "skip-{n}m", n시간이내 유효 : "skip-{n}h"
-        public string? same_order { get; set; }
+        public string? Same_Order { get; set; }
         // 주문 시간대 설정
         // hh:mm-hh:mm : 00:30-02:00
-        // public string? Trading_time { get; set; }
-        public string? trading_startTime { get; set; }
-        public string? trading_endTime { get; set; }
+        public string? Trading_Time { get; set; }
 
         // 지연시간
-        // 실제 표시는 초단위, 소수점 한자리까지만 입력가능
-        public Decimal? delay { get; set; }
+        // 소수점 1번째 자리까지 입력가능
+        public decimal? delay { get; set; }
         // 메모
         // 사용자 검색용 메모
+        [JsonIgnore]
         public string? memo { get; set; }
+        // 주문ID
+        public string? orderID { get; set; }
     }
 
     public class CreateMessageModelTypeList
