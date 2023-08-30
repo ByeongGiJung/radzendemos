cdae7ccf9bacacf1c8e4086a8b69d4b531a9e042
jbg <qudrl0284@gmail.com>
2023-08-30 19:04:59 +0900

    230830

--- Data/Models/CreateMessageModel.cs
+++ Data/Models/CreateMessageModel.cs
@@ -1,26 +1,85 @@
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace TempTitle.Data.ViewModel.CreateMessageModel
 {
     public class CreateMessageModel
     {
         // 거래소
-        public string Exchange { get; set; }
+        // Bitget 현물 : bitget-spot, 선물 : bitget
+        public string _exchange;
+        [JsonIgnore]
+        public string RawExchange
+        {
+            get => _exchange;
+            set => _exchange = value;
+        }
+        public string Exchange => _exchange switch
+        {
+            "Bitget 현물" => "bitget-spot",
+            "Bitget 선물" => "bitget-spot",
+            _ => throw new NotImplementedException()
+        };
+
         // 계정
+        // TODO : 계정 APIKEY 관련해서 정해지면 수정
         public string Account { get; set; }
         // 거래종목
+        // TODO : 확인 후 추가
         public string Symbol { get; set ; }
         // 거래마켓
+        // TODO : 확인 후 추가
         public string Market { get; set; }
+
         // 주문유형
         // 지정가 : limit, 시장가 : market
-        public string Type { get; set; }
+        public string _type;
+        [JsonIgnore]
+        public string RawType
+        {
+            get => _type;
+            set => _type = value;
+        }
+        public string Type => _type switch
+        {
+            "지정가" => "limit",
+            "시장가" => "market",
+            _ => throw new NotImplementedException()
+        };
+
         // 주문방법
         // 매수 : buy, 매도 : sell
-        public string Side { get; set; }
+        public string _side;
+        [JsonIgnore]
+        public string RawSide
+        {
+            get => _side;
+            set => _side = value;
+        }
+        public string Side  => _side switch
+        {
+            "매수" => "buy",
+            "매도" => "sell",
+            _ => throw new NotImplementedException()
+        };
+
         // 주문가격
         // LAST, BID, ASK, string(소수점포함 실수로 작성가능 / 숫자가 아니면 작성x하고 오류메시지 출력)
-        public string Price { get; set; }
+        public string _price;
+        [JsonIgnore]
+        public string RawPrice
+        {
+            get => _price;
+            set => _price = value;
+        }
+        public string Price => _price switch
+        {
+            "LAST(마지막 체결가격)" => "last",
+            "BID(매수호가에서 최고가격)" => "bid",
+            "ASK(매도호가에서 최저가격)" => "ask",
+            "직접입력" => "self",
+            _ => throw new NotImplementedException()
+        };
+
         // 주문가격대비 퍼센트(%)
         // 선택사항 : 왼쪽 대비 싸게 매수할 경우 -n(%)을 입력(마이너스값)
         // 소수점 2번째 자리까지 가능
@@ -36,12 +95,18 @@ namespace TempTitle.Data.ViewModel.CreateMessageModel
 
         // 기준자산
         // 잔액대비 : 없음, 총자산대비 : "equity" : "y"
-        public string? Equity { get; set; }
+        public string? _equity;
+        [JsonIgnore]
         public string? RawEquity
         {
-            get => Equity;
-            set => Equity = value;
+            get => _equity;
+            set => _equity = value;
         }
+        public string? Equity
+        {
+            get => _equity == "총자산대비" ? "y" : null;
+        }
+
         // 주문수량(%)
         // 소수 2번째 자리까지 가능
         public decimal? Bal_Pct { get; set; }
@@ -70,17 +135,17 @@ namespace TempTitle.Data.ViewModel.CreateMessageModel
 
     public class CreateMessageModelTypeList
     {
-        public static List<string> Exchanges => new List<string> { "비트겟" };
+        public static List<string> Exchanges => new List<string> { "Bitget 현물", "Bitget 선물" };
         public static List<string> Accounts => new List<string> { "기본계정" };
         // TODO : 확인 후 추가필요
         public static List<string> Symbols => new List<string> { "1INCH/KRW", "AAVE/KRW" };
         public static List<string> Markets => new List<string> { "KRW" };
-        public static List<string> Types => new List<string> { "limit", "market" };
-        public static List<string> Sides => new List<string> { "buy", "sell" };
+        public static List<string> Types => new List<string> { "지정가", "시장가" };
+        public static List<string> Sides => new List<string> { "매수", "매도" };
         // TODO : 거래소별로 화폐종류가 다름
         public static List<string> Amount_Types => new List<string> { "Current", "KRW" };
         public static List<string> Equity_Types => new List<string> { "잔액대비", "총자산대비" };
-        public static List<string> Prices => new List<string> { "LAST", "BID", "ASK", "SELF" };
+        public static List<string> Prices => new List<string> { "LAST(마지막 체결가격)", "BID(매수호가에서 최고가격)", "ASK(매도호가에서 최저가격)", "직접입력" };
         public static List<string> Open_Orders => new List<string> { "cancel", "cancel-buy", "cancel-sell", "cancel-close" };
     }
 }
